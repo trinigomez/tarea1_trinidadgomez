e9e5400bf4d9390921708346244de52e4a594876
trinigomez <tgomez4@uc.cl>
2021-04-08 12:44:21 -0400

    episodios breaking bad

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/series", to: "series#index"
   get "/episodes", to: "episodes#index"
-  get "/breakingbad", to: "breaking_bad#index"
+  #get "/breakingbad", to: "breaking_bad#index"
+  #get '/breakingbad/:id', to: "breaking_bad#show"
   get "/bettercallsaul", to: "better_call_saul#index"
+  resources :breaking_bad, only: [:index, :show]
   root 'series#index', as: 'home'
 end
