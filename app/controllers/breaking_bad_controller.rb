7b6949d6e1e95ddc9b4f9b89f1a69c6666928919
trinigomez <tgomez4@uc.cl>
2021-04-09 10:28:18 -0400

    barra de busqueda

--- app/controllers/breaking_bad_controller.rb
+++ app/controllers/breaking_bad_controller.rb
@@ -6,6 +6,7 @@ class BreakingBadController < ApplicationController
     @episodes = JSON.parse(HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Breaking+Bad').to_s)
     # tal vez haya que arreglar despues
     @num_seasons = @episodes[-1]["season"].to_i # indica que son 5 temporadas
+    @show_season = params[:season]
   end
 
   def show
--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,8 +1,14 @@
 class CharactersController < ApplicationController
 
     def show
-        puts 'llegue hasta aca'
         name = params[:name].split.join("+")
         @character = JSON.parse(HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters?name=' + name).to_s)[0]
+        @quotes = JSON.parse(HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/quote?author=' + name).to_s)
+    end
+
+    def search
+        @busqueda = params[:q]
+        search = params[:q].split.join("+")
+        @results = JSON.parse(HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters?name=' + search).to_s)
     end
 end
