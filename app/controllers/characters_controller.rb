92c0f808f69a9ee88b399086ba01d042452a9583
trinigomez <tgomez4@uc.cl>
2021-04-09 11:27:02 -0400

    paginacion busqueda

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -10,5 +10,12 @@ class CharactersController < ApplicationController
         @busqueda = params[:q]
         search = params[:q].split.join("+")
         @results = JSON.parse(HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters?name=' + search).to_s)
+        while true
+            if @results.length == 10
+                @results = @results + JSON.parse(HTTP.get('https://tarea-1-breaking-bad.herokuapp.com/api/characters?name=' + search + '&limit=10&offset=10').to_s)
+            else
+                break
+            end
+        end
     end
 end
