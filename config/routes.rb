04ea30b05208d64d594b18fd19b3b31260eb03a1
trinigomez <tgomez4@uc.cl>
2021-04-09 11:10:27 -0400

    listo flujo, falta hacer css

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   #resources :breaking_bad, only: [:index, :show]
   get "/breaking_bad(/season/:season)", to: "breaking_bad#index", :as => 'breaking_bad_index'
   get "/breaking_bad/:id", to: "breaking_bad#show", :as => 'breaking_bad'
-  resources :better_call_saul, only: [:index, :show]
+  
+  resources :better_call_saul, only: [:show]
+  get "/better_call_saul(/season/:season)", to: "better_call_saul#index", :as => 'better_call_saul_index'
+
   get "/characters/:name", to: "characters#show", :as => 'character'
   
   get "/search", to: "characters#search", :as => 'search'
